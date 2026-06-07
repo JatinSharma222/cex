11eff9b9e6042e8fa06b730ba43990dd46bf1ee8
JatinSharma222 <sharma.jatin2206@gmail.com>
2026-06-07 23:20:58 +0530

    feat: initilized the basic auth(singin)

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -32,3 +32,35 @@ export async function signup(req:Request, res: Response): Promise<void> {
         })
     }
 }
+
+export async function signin(req: Request, res: Response): Promise<void> {
+  const parsedBody = authSchema.safeParse(req.body);
+  if (!parsedBody.success) {
+    sendValidationError(res, parsedBody.error);
+    return;
+  }
+
+  const { username, password } = parsedBody.data;
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  });
+
+  if (!user) {
+    res.status(401).json({ error: "invalid credentials" });
+    return;
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    res.status(401).json({ error: "invalid credentials" });
+    return;
+  }
+
+  res.json({
+    token: createToken({ userId: user.id }),
+    userId: user.id,
+    username: user.username,
+  });
+
+}
