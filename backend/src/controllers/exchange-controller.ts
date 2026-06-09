45b42ff900317e7c2dcc99a37fbe170196773015
JatinSharma222 <sharma.jatin2206@gmail.com>
2026-06-09 23:25:13 +0530

    feat: added getBalance endpoint

--- backend/src/controllers/exchange-controller.ts
+++ backend/src/controllers/exchange-controller.ts
@@ -57,7 +57,19 @@ export async function getDepth(req: Request, res: Response): Promise<void> {
     );
 }
 
-export function getBalance() {}
+export async function getBalance(req: Request, res: Response): Promise<void> {
+  const engineResponse = await sendToEngine("get_user_balance", {
+    userId: getUserId(req),
+  });
+
+  res.status(engineResponse.ok ? 200 : 400).json(
+    engineResponse.ok
+      ? engineResponse.data
+      : {
+          error: engineResponse.error,
+        },
+  );
+}
 
 export function getOrder() {}
 
